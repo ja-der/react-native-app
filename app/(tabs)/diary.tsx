c5bc38f649e64d9280538fe273c5e58ef2c56a95
jason <jason_der@outlook.com>
2025-04-29 22:27:28 -0400

    diary poc

--- app/(tabs)/diary.tsx
+++ app/(tabs)/diary.tsx
@@ -1,18 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-type Entry = {
-  id: number;
-  restaurant: string;
-  ethnicity: string;
-  iamge: string;
-  description: string;
-};
-
 const Diary = () => {
-
-  const renderCard = ({item}: {item:Entry}) =>{}
-
   return (
     <View>
       <Text>diary</Text>
